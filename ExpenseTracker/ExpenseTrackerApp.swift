77de451ed1084b739d29142eab047518864c64a6
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-11-18 12:02:30 -0600

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Dmytro Ryshchuk on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
